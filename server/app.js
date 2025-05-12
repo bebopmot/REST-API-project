7945e0a013b4e4b21101d6ab1cfec4fa59e9e789
bebopmot <bebopmotinc@gmail.com>
2025-05-12 17:25:34 +0200

    initial commit react app students and cohorts pages and cors attempt

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const students = require("./students.json")
+const cohorts = require("./cohorts.json")
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -20,17 +22,27 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(cors({ origin: 'http://localhost:5005' }));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/students",cors(),(req, res, next)=>{
+res.json(students)
+});
+
+app.get("/api/cohorts",(req, res, next)=>{
+res.json(cohorts)
+});
+
+
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
