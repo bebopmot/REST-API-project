aeb8f9bfc82e1a63067b043e864f08bae3ab14b3
“nekosaege” <nekofreiheit@gmx.de>
2025-05-18 13:20:51 +0200

    sign up improve

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -11,32 +11,43 @@ router.post("/signup", (req, res, next) => {
 	if (!email || !password || !firstName || !lastName) {
 		return res.status(400).json({ message: "All fields are required" });
 	}
-User.findOne({email})
-.then((foundUser)=>{
-    if (foundUser){
-        res.status(400).json({ message: "User already exists" });
-        return;
-    }
-    const salt = bcrypt.genSaltSync(saltRounds);
-	const passwordHash = bcrypt.hashSync(password, salt);
 
-	User.create({ email, passwordHash, firstName, lastName })
-		.then((createdUser) => {
-			const { email, firstName, lastName, _id } = createdUser;
-			const user = { email, firstName, lastName, _id };
-			res.status(201).json(user);
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!emailRegex.test(email)) {
+		res.status(400).json({ message: "Not a valid email" });
+		return;
+	}
+
+	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    if(!passwordRegex.test(password)){
+        res.status(400).json({ message: "Password needs to contain at leat 8 characters, at least 1 number, 1 lowercase, 1 uppercase and 1 special character" });
+		return;
+    }
+
+	User.findOne({ email })
+		.then((foundUser) => {
+			if (foundUser) {
+				res.status(400).json({ message: "User already exists" });
+				return;
+			}
+			const salt = bcrypt.genSaltSync(saltRounds);
+			const passwordHash = bcrypt.hashSync(password, salt);
+
+			return User.create({ email, passwordHash, firstName, lastName })
+				.then((createdUser) => {
+					const { email, firstName, lastName, _id } = createdUser;
+					const user = { email, firstName, lastName, _id };
+					res.status(201).json(user);
+				})
+				.catch((error) => {
+					console.log("Error trying to create an account", error);
+					res.status(500).json({ message: "Internal Server Error" });
+				});
 		})
 		.catch((error) => {
 			console.log("Error trying to create an account", error);
 			res.status(500).json({ message: "Internal Server Error" });
 		});
-})
-.catch((error) => {
-    console.log("Error trying to create an account", error);
-    res.status(500).json({ message: "Internal Server Error" });
-});
-
-	
 });
 
 module.exports = router;
